1ea85d7a070e59d7997ac45c98d45fc446364d3a
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-04-19 14:09:22 +0800

    basic parser for mp-import

---
+++ parse.py
@@ -0,0 +1,40 @@
+from pyparsing import Group, OneOrMore, Opt, Word, alphanums
+
+EXAMPLES = [
+    "afi ipv6.unicast from AS9002 accept ANY",
+    "afi ipv6.unicast from AS9002 from AS2356 accept ANY",
+    "afi ipv6.unicast from AS6939 action pref=100; accept ANY",
+    "afi ipv6.unicast from AS21127 action pref=100; accept AS-ZSTTK6-SET;",
+    "afi ipv6.unicast from AS21127 action pref=100; med=0; accept AS-ZSTTK6-SET;",
+    # "afi ipv6 from AS1213 accept { ::/0 }",
+    # "afi ipv6.unicast from AS1299 action pref = 200; accept ANY AND NOT {0.0.0.0/0};",
+    # "afi ipv4.unicast from AS6682 at 109.68.121.1 action pref=65435; med=0; community.append(8226:1102); accept ANY AND {0.0.0.0/0^0-24}",
+]
+
+field = Word(alphanums + ".-=")
+semicolon = Word(";").suppress()
+protocol = "protocol" + field("protocol-1")
+into_protocol = "into" + field("protocol-2")
+afi = "afi" + field("afi-list")
+actions = "action" + Group(OneOrMore(field + semicolon)).set_results_name("actions")
+peering = Group(
+    "from"
+    # TODO: deal with <mp-peering>.
+    + field("mp-peering")
+    + Opt(actions)
+)
+lex = (
+    Opt(protocol)
+    + Opt(into_protocol)
+    + Opt(afi)
+    + Group(OneOrMore(peering)).set_results_name("from")
+    + "accept"
+    # TODO: deal with <mp-filter>
+    + field("mp-filter")
+    + Opt(semicolon)
+)
+
+for example in EXAMPLES:
+    print(f"\n{example} ->")
+    results = lex.parseString(example)
+    print(results.as_dict())

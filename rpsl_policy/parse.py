2a3dbb5582c8d99c2565ad4d27c512912c457e70
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-04-20 20:36:50 +0800

    basic mp-peering parsing logic w/o example/test

--- rpsl_policy/parse.py
+++ rpsl_policy/parse.py
@@ -22,6 +22,9 @@ semicolon = Word(";").suppress()
 from_kw = CaselessKeyword("from")
 action_kw = CaselessKeyword("action")
 accept_kw = CaselessKeyword("accept")
+and_kw = CaselessKeyword("and")
+or_kw = CaselessKeyword("or")
+except_kw = CaselessKeyword("except")
 protocol = CaselessKeyword("protocol") + field("protocol-1")
 into_protocol = "into" + field("protocol-2")
 afi = CaselessKeyword("afi") + delimited_list(
@@ -31,8 +34,8 @@ action = field_w_space + semicolon
 actions = action_kw + Group(
     ZeroOrMore(action + ~(from_kw | accept_kw)) + action
 ).set_results_name("actions")
-mp_peering = Group(ZeroOrMore(field + ~(action_kw | from_kw | accept_kw)) + field)
-peering = Group(from_kw + mp_peering.set_results_name("mp-peering") + Opt(actions))
+mp_peering_raw = Group(ZeroOrMore(field + ~(action_kw | from_kw | accept_kw)) + field)
+peering = Group(from_kw + mp_peering_raw.set_results_name("mp-peering") + Opt(actions))
 import_factor = (
     Group(OneOrMore(peering)).set_results_name("from")
     + accept_kw
@@ -61,3 +64,12 @@ lex = Opt(protocol) + Opt(into_protocol) + afi_import_expression
 
 # TODO: parse <mp-filter>.
 # TODO: parse <mp-peering>.
+operator_except = and_kw | or_kw | except_kw
+as_expression = Group(field + ZeroOrMore(operator_except + field))
+# Works under my assumption that inet-rtr names and rtr-set names match `field`.
+mp_router_expression = as_expression
+mp_peering = Group(
+    as_expression("as-expression")
+    + Opt(mp_router_expression("mp-router-expression-1"))
+    + Opt("at" + mp_router_expression("mp-router-expression-2"))
+) | field("peering-set-name")

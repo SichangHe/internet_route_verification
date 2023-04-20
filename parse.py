62f247ead94dadaab5ec645fb073a35ace51908a
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-04-20 08:41:32 +0800

    get full <mp-peering>

--- parse.py
+++ parse.py
@@ -1,4 +1,13 @@
-from pyparsing import Group, OneOrMore, Opt, Word, alphanums
+from pyparsing import (
+    Group,
+    Keyword,
+    OneOrMore,
+    Opt,
+    Word,
+    ZeroOrMore,
+    alphanums,
+    printables,
+)
 
 EXAMPLES = [
     "afi ipv6.unicast from AS9002 accept ANY",
@@ -8,22 +17,21 @@ EXAMPLES = [
     "afi ipv6.unicast from AS21127 action pref=100; med=0; accept AS-ZSTTK6-SET;",
     "afi ipv6 from AS1213 accept { ::/0 }",
     "afi ipv6.unicast from AS1299 action pref=200; accept ANY AND NOT {0.0.0.0/0};",
-    # "afi ipv6.unicast from AS1299 action pref = 200; accept ANY AND NOT {0.0.0.0/0};",
-    # "afi ipv4.unicast from AS6682 at 109.68.121.1 action pref=65435; med=0; community.append(8226:1102); accept ANY AND {0.0.0.0/0^0-24}",
+    "afi ipv6.unicast from AS1299 action pref = 200; accept ANY AND NOT {0.0.0.0/0};",
+    "afi ipv4.unicast from AS6682 at 109.68.121.1 action pref=65435; med=0; community.append(8226:1102); accept ANY AND {0.0.0.0/0^0-24}",
 ]
 
-field = Word(alphanums + "-+=.")
+field = Word(printables, exclude_chars=";")
 semicolon = Word(";").suppress()
 protocol = "protocol" + field("protocol-1")
 into_protocol = "into" + field("protocol-2")
 afi = "afi" + field("afi-list")
 actions = "action" + Group(OneOrMore(field + semicolon)).set_results_name("actions")
-peering = Group(
-    "from"
-    # TODO: deal with <mp-peering>.
-    + field("mp-peering")
-    + Opt(actions)
+mp_peering = Group(
+    ZeroOrMore(field + ~(Keyword("action") | Keyword("from") | Keyword("accept")))
+    + field
 )
+peering = Group("from" + mp_peering.set_results_name("mp-peering") + Opt(actions))
 lex = (
     Opt(protocol)
     + Opt(into_protocol)
@@ -35,8 +43,9 @@ lex = (
 )
 
 # TODO: parse <mp-filter>.
+# TODO: parse <mp-peering>.
 
 for example in EXAMPLES:
-    print(f"\n{example} ->")
-    results = lex.parseString(example)
-    print(results.as_dict())
+    success, results = lex.run_tests(example, full_dump=False)
+    if success:
+        print(results[0][1].as_dict())  # type: ignore

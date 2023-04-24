9702405a5bae2ebde7c0a93180b2724dca3814dd
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-04-24 14:31:34 +0800

    mp-export db test

---
+++ rpsl_policy/tests/mp_export_w_db.py
@@ -0,0 +1,44 @@
+from io import TextIOWrapper
+from random import choices
+
+from ..lex import mp_export
+from ..lines import io_wrapper_lines, lines_continued
+
+
+def parse_mp_export(line: str, verbose: bool = False):
+    _, value = line.split(":", maxsplit=1)
+    value = value.strip()
+    if verbose:
+        success, results = mp_export.run_tests(value, full_dump=False)
+        if success:
+            print(results[0][1].as_dict())  # type: ignore
+    elif not mp_export.matches(value):
+        # Match failed.
+        parse_mp_export(line, True)
+
+
+def parse_statement(statement: str, verbose: bool = False):
+    if ":" not in statement or not statement.startswith("mp-export"):
+        return 0
+    parse_mp_export(statement, verbose)
+    return 1
+
+
+def read_db_test_parser(db: TextIOWrapper):
+    line: str = ""
+    n_mp_export = 0
+    db_lines = io_wrapper_lines(db)
+    for line in lines_continued(db_lines):
+        # 1% chance verbose.
+        verbose = choices((True, False), (1, 99))[0]
+        n_mp_export += parse_statement(line, verbose)
+    print(f"Read {n_mp_export} mp-exports.")
+
+
+def main():
+    with open("data/ripe.db", "r", encoding="latin-1") as db:
+        read_db_test_parser(db)
+
+
+if __name__ == "__main__":
+    main()

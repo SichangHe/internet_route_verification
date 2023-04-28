edf56fcae669c7b7a5286e840037fd9d3b3c8c7c
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-04-28 18:22:33 +0800

    save all in memory db test

---
+++ rpsl_policy/tests/save_lex_db.py
@@ -0,0 +1,47 @@
+from io import TextIOWrapper
+
+from pyparsing import ParseException
+
+from ..lex import mp_import
+from ..lines import io_wrapper_lines, lines_continued
+
+mp_imports = []
+mp_exports = []
+imports = []
+exports = []
+
+
+def parse_mp_import(line: str):
+    _, value = line.split(":", maxsplit=1)
+    value = value.strip()
+    return mp_import.parse_string(value)
+
+
+def parse_statement(statement: str):
+    try:
+        if statement.startswith("mp-import:"):
+            mp_imports.append(parse_mp_import(statement))
+        if statement.startswith("mp-export:"):
+            mp_exports.append(parse_mp_import(statement))
+        if statement.startswith("import:"):
+            imports.append(parse_mp_import(statement))
+        if statement.startswith("export:"):
+            exports.append(parse_mp_import(statement))
+    except ParseException as err:
+        print(f"ERROR: {err}")
+
+
+def read_db_test_parser(db: TextIOWrapper):
+    line: str = ""
+    db_lines = io_wrapper_lines(db)
+    for line in lines_continued(db_lines):
+        parse_statement(line)
+
+
+def main():
+    with open("data/ripe.db", "r", encoding="latin-1") as db:
+        read_db_test_parser(db)
+
+
+if __name__ == "__main__":
+    main()

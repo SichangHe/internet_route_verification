17eed969665f95df87b6c67243a8cc7072f5850d
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-05-27 10:44:03 +0800

    check policy use Vec w/o Option & early return on Report::Good

--- route_policy_cmp/src/bgp/cmp.rs
+++ route_policy_cmp/src/bgp/cmp.rs
@@ -17,7 +17,7 @@ pub fn compare_line_w_dump(line: &str, dump: &Dump) -> Result<Vec<Report>> {
             if let (AsPathEntry::Seq(from), AsPathEntry::Seq(to)) = (from, to) {
                 compare_pair_w_dump(*from, *to, dump, prefix, &communities)
             } else {
-                vec![Report::Skipped(format!(
+                vec![Report::Skip(format!(
                     "Skipping BGP pair {from}, {to} with set."
                 ))]
             }
@@ -35,11 +35,11 @@ pub fn compare_pair_w_dump(
 ) -> Vec<Report> {
     let mut from_report = match dump.aut_nums.get(&from) {
         Some(from_an) => check_compliant(&from_an.exports, to, prefix, communities),
-        None => vec![Report::Skipped(format!("{from} is not a recorded AutNum"))],
+        None => vec![Report::Skip(format!("{from} is not a recorded AutNum"))],
     };
     let to_report = match dump.aut_nums.get(&to) {
         Some(to_an) => check_compliant(&to_an.imports, from, prefix, communities),
-        None => vec![Report::Skipped(format!("{to} is not a recorded AutNum"))],
+        None => vec![Report::Skip(format!("{to} is not a recorded AutNum"))],
     };
     from_report.extend(to_report);
     from_report
@@ -51,17 +51,28 @@ pub fn check_compliant(
     prefix: IpNet,
     communities: &[&str],
 ) -> Vec<Report> {
-    let mut reports = Vec::with_capacity(2);
-    if let Some(specific_report) = match prefix {
+    let mut reports = Vec::new();
+    let specific_report = match prefix {
         IpNet::V4(_) => check_casts_compliant(&policy.ipv4, accept_num, prefix, communities),
         IpNet::V6(_) => check_casts_compliant(&policy.ipv6, accept_num, prefix, communities),
-    } {
-        reports.push(specific_report);
+    };
+    if let Some(Report::Good) = specific_report.first() {
+        // This route is good.
+        return specific_report;
     }
-    if let Some(general_report) =
-        check_casts_compliant(&policy.any, accept_num, prefix, communities)
-    {
-        reports.push(general_report);
+    reports.extend(specific_report);
+
+    let general_report = check_casts_compliant(&policy.any, accept_num, prefix, communities);
+    if let Some(Report::Good) = general_report.first() {
+        // This route is good.
+        return general_report;
+    }
+    reports.extend(general_report);
+
+    if reports.is_empty() {
+        reports.push(Report::NoMatch(format!(
+            "No policy in {policy:?} matches AS{accept_num} from {prefix}"
+        )));
     }
     reports
 }
@@ -71,17 +82,21 @@ pub fn check_casts_compliant(
     accept_num: usize,
     prefix: IpNet,
     communities: &[&str],
-) -> Option<Report> {
+) -> Vec<Report> {
+    let mut reports = Vec::new();
     // TODO: How do we know the casts?
     for entry in [&casts.multicast, &casts.unicast, &casts.any]
         .into_iter()
         .flatten()
     {
-        if let Some(report) = check_entry_compliant(entry, accept_num, prefix, communities) {
-            return Some(report);
+        let report = check_entry_compliant(entry, accept_num, prefix, communities);
+        if let Some(Report::Good) = report.first() {
+            // This route is good.
+            return vec![Report::Good];
         }
+        reports.extend(report);
     }
-    None
+    reports
 }
 
 pub fn check_entry_compliant(
@@ -89,10 +104,12 @@ pub fn check_entry_compliant(
     accept_num: usize,
     prefix: IpNet,
     communities: &[&str],
-) -> Option<Report> {
+) -> Vec<Report> {
     todo!()
 }
 
 pub enum Report {
-    Skipped(String),
+    Skip(String),
+    Good,
+    NoMatch(String),
 }

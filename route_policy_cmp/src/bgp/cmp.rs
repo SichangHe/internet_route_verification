d8815d6930a564a451a12ef8fa7a17f39d0fea7f
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-05-27 10:21:19 +0800

    skeleton: check casts,entry

--- route_policy_cmp/src/bgp/cmp.rs
+++ route_policy_cmp/src/bgp/cmp.rs
@@ -1,7 +1,10 @@
 use anyhow::Result;
 use ipnet::IpNet;
 
-use crate::parse::{lex::Dump, mp_import::Versions};
+use crate::parse::{
+    lex::Dump,
+    mp_import::{Casts, Entry, Versions},
+};
 
 use super::map::{parse_table_dump, AsPathEntry};
 
@@ -31,23 +34,62 @@ pub fn compare_pair_w_dump(
     communities: &[&str],
 ) -> Vec<Report> {
     let mut from_report = match dump.aut_nums.get(&from) {
-        Some(from_an) => check_complient(&from_an.exports, to, prefix, communities),
+        Some(from_an) => check_compliant(&from_an.exports, to, prefix, communities),
         None => vec![Report::Skipped(format!("{from} is not a recorded AutNum"))],
     };
     let to_report = match dump.aut_nums.get(&to) {
-        Some(to_an) => check_complient(&to_an.imports, from, prefix, communities),
+        Some(to_an) => check_compliant(&to_an.imports, from, prefix, communities),
         None => vec![Report::Skipped(format!("{to} is not a recorded AutNum"))],
     };
     from_report.extend(to_report);
     from_report
 }
 
-pub fn check_complient(
+pub fn check_compliant(
     policy: &Versions,
     accept_num: usize,
     prefix: IpNet,
     communities: &[&str],
 ) -> Vec<Report> {
+    let mut reports = Vec::with_capacity(2);
+    if let Some(specific_report) = match prefix {
+        IpNet::V4(_) => check_casts_compliant(&policy.ipv4, accept_num, prefix, communities),
+        IpNet::V6(_) => check_casts_compliant(&policy.ipv6, accept_num, prefix, communities),
+    } {
+        reports.push(specific_report);
+    }
+    if let Some(general_report) =
+        check_casts_compliant(&policy.any, accept_num, prefix, communities)
+    {
+        reports.push(general_report);
+    }
+    reports
+}
+
+pub fn check_casts_compliant(
+    casts: &Casts,
+    accept_num: usize,
+    prefix: IpNet,
+    communities: &[&str],
+) -> Option<Report> {
+    // TODO: How do we know the casts?
+    for entry in [&casts.multicast, &casts.unicast, &casts.any]
+        .into_iter()
+        .flatten()
+    {
+        if let Some(report) = check_entry_compliant(entry, accept_num, prefix, communities) {
+            return Some(report);
+        }
+    }
+    None
+}
+
+pub fn check_entry_compliant(
+    entry: &Entry,
+    accept_num: usize,
+    prefix: IpNet,
+    communities: &[&str],
+) -> Option<Report> {
     todo!()
 }
 

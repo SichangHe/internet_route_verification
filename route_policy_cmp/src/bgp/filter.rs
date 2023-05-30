f5f42fe2f7a6d2895b0f5b6f65cf99db6926cf59
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-05-30 11:32:27 +0800

    skeleton: filter route&AS set name;filter_prefixes take IntoIterator

--- route_policy_cmp/src/bgp/filter.rs
+++ route_policy_cmp/src/bgp/filter.rs
@@ -1,6 +1,11 @@
 use crate::parse::{
     address_prefix::AddrPfxRange,
-    filter::Filter::{self, *},
+    aut_sys::AsName,
+    filter::{
+        Filter::{self, *},
+        RegexOperator,
+    },
+    set::RouteSetMember,
 };
 
 use super::{
@@ -22,9 +27,9 @@ impl<'a> CheckFilter<'a> {
             FilterSetName(_) => todo!(),
             Any => Some(Good),
             AddrPrefixSet(prefixes) => self.filter_prefixes(prefixes),
-            RouteSetName(_) => todo!(),
+            RouteSetName(name) => self.filter_route_set_name(name),
             AsNum(num, _) => (*num == self.accept_num).then_some(Good), // TODO: what about the operator?
-            AsSet(_, _) => todo!(),
+            AsSet(name, op) => self.filter_as_set_name(name, op),
             AsPathRE(_) => todo!(),
             PeerAs => todo!(),
             And { left, right } => self.filter_and(self.check(left)?, right),
@@ -39,15 +44,63 @@ impl<'a> CheckFilter<'a> {
         }
     }
 
-    fn filter_prefixes(&self, prefixes: &[AddrPfxRange]) -> Option<Report> {
+    fn filter_prefixes<I>(&self, prefixes: I) -> Option<Report>
+    where
+        I: IntoIterator<Item = &'a AddrPfxRange>,
+    {
         prefixes
-            .iter()
+            .into_iter()
             .any(|prefix| prefix.contains(&self.compare.prefix))
             .then_some(Good)
     }
 
+    fn filter_route_set_name(&self, name: &str) -> Option<Report> {
+        let route_set = match self.compare.dump.route_sets.get(name) {
+            Some(r) => r,
+            None => return Some(Skip(format!("{name} is not a recorded Route Set"))),
+        };
+        let mut report = None;
+        for member in &route_set.members {
+            match self.filter_route_set_member(member) {
+                Some(Good) => return Some(Good),
+                new_report => report = report::or(report, new_report),
+            }
+        }
+        report
+    }
+
+    fn filter_route_set_member(&self, member: &RouteSetMember) -> Option<Report> {
+        match member {
+            RouteSetMember::Range(prefix) => self.filter_prefixes([prefix]),
+            RouteSetMember::Name(name) => self.filter_route_set_name(name),
+            RouteSetMember::NameOp(_, _) => todo!(),
+        }
+    }
+
+    fn filter_as_set_name(&self, name: &str, op: &RegexOperator) -> Option<Report> {
+        let as_set = match self.compare.dump.as_sets.get(name) {
+            Some(r) => r,
+            None => return Some(Skip(format!("{name} is not a recorded AS Set"))),
+        };
+        let mut report = None;
+        for as_name in &as_set.members {
+            match self.filter_as_name(as_name, op) {
+                Some(Good) => return Some(Good),
+                new_report => report = report::or(report, new_report),
+            }
+        }
+        report
+    }
+
+    fn filter_as_name(&self, as_name: &AsName, op: &RegexOperator) -> Option<Report> {
+        match as_name {
+            AsName::Num(_) => todo!(),
+            AsName::Set(_) => todo!(),
+            AsName::Illegal(_) => todo!(),
+        }
+    }
+
     fn filter_and(&self, left_report: Report, right: &Filter) -> Option<Report> {
-        // let left_report = self.check(left)?;
         match right {
             And { left, right } => self.filter_and(left_report & self.check(left)?, right),
             Or { left, right } => self.filter_or(left, right, Some(left_report)),

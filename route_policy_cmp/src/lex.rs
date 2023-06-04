245bd5aac33fd4a2d2d15dfd6a497f5ae0946f30
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-06-04 10:04:23 +0800

    rewrite python rpsl_policy/lines in rust

--- route_policy_cmp/src/lex.rs
+++ route_policy_cmp/src/lex.rs
@@ -2,6 +2,7 @@ pub mod action;
 pub mod community;
 pub mod dump;
 pub mod filter;
+pub mod lines;
 pub mod mp_import;
 pub mod peering;
 pub mod rpsl_object;
---
+++ route_policy_cmp/src/lex/lines.rs
@@ -0,0 +1,178 @@
+use std::borrow::Cow;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::mem;
+
+use lazy_regex::regex_replace_all;
+use log::error;
+
+const CONTINUATION_CHARS: [&str; 3] = [" ", "+", "\t"];
+
+pub fn io_wrapper_lines(reader: BufReader<File>) -> impl Iterator<Item = String> {
+    reader.lines().map_while(Result::ok)
+}
+
+pub struct LinesContinued<I>
+where
+    I: Iterator<Item = String>,
+{
+    lines: I,
+    last_line: String,
+}
+
+impl<I> Iterator for LinesContinued<I>
+where
+    I: Iterator<Item = String>,
+{
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for line in self.lines.by_ref() {
+            // Remove comments.
+            let line = line.split('#').next().unwrap().trim();
+            // Handle continuation lines.
+            if CONTINUATION_CHARS.iter().any(|&ch| line.starts_with(ch)) {
+                self.last_line.push(' ');
+                self.last_line.push_str(line[1..].trim());
+            } else {
+                // Not a continuation line.
+                return Some(mem::replace(&mut self.last_line, line.into()));
+            }
+        }
+        (!self.last_line.is_empty()).then(|| mem::take(&mut self.last_line))
+    }
+}
+
+/// Merge continued RPSL lines in `raw_lines` into single lines according to
+/// prefix continuation characters and yield them one by one.
+/// Strip and ignore comments. Ignore empty lines."""
+pub fn lines_continued(
+    raw_lines: impl IntoIterator<Item = String>,
+) -> impl Iterator<Item = String> {
+    LinesContinued {
+        lines: raw_lines.into_iter(),
+        last_line: String::new(),
+    }
+}
+
+pub fn dedup_whitespace(string: &str) -> Cow<str> {
+    regex_replace_all!(r"\s+", string, |_| " ")
+}
+
+pub fn cleanup_right_whitespace(string: &str) -> Cow<str> {
+    dedup_whitespace(string.trim_end())
+}
+
+pub fn cleanup_whitespace(string: &str) -> Cow<str> {
+    dedup_whitespace(string.trim())
+}
+
+#[derive(Debug)]
+pub struct RPSLObject {
+    pub class: String,
+    pub name: String,
+    pub body: String,
+}
+
+impl RPSLObject {
+    fn new(class: String, name: String, body: String) -> Self {
+        Self { class, name, body }
+    }
+}
+
+pub struct RpslObjects<I>
+where
+    I: Iterator<Item = String>,
+{
+    lines: I,
+    class: String,
+    name: String,
+    body: String,
+    new: bool,
+}
+
+impl<I> Iterator for RpslObjects<I>
+where
+    I: Iterator<Item = String>,
+{
+    type Item = RPSLObject;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for line in self.lines.by_ref() {
+            let line = cleanup_right_whitespace(&line);
+            if line.is_empty() {
+                // Empty line suggests the end of the last object.
+                if self.new {
+                    continue;
+                }
+                self.new = true;
+                // Yield the last object.
+                return Some(RPSLObject::new(
+                    mem::take(&mut self.class),
+                    mem::take(&mut self.name),
+                    mem::take(&mut self.body),
+                ));
+            }
+            if !self.new {
+                self.body.push_str(&line);
+                self.body.push('\n');
+                continue;
+            }
+            // Start of new object.
+            if !line.contains(':') {
+                error!("Invalid line for start of RPSL object: `{line}`.");
+                continue;
+            }
+            let mut parts = line.splitn(2, ':').map(cleanup_whitespace);
+            self.class = parts.next().unwrap().into();
+            self.name = parts.next().unwrap().into();
+            self.new = false;
+            self.body.clear();
+        }
+        if self.new {
+            None
+        } else {
+            // The last line is not empty.
+            Some(RPSLObject::new(
+                mem::take(&mut self.class),
+                mem::take(&mut self.name),
+                mem::take(&mut self.body),
+            ))
+        }
+    }
+}
+
+/// Combine lines from an iterator into RPSL objects.
+pub fn rpsl_objects<I>(lines: I) -> impl Iterator<Item = RPSLObject>
+where
+    I: IntoIterator<Item = String>,
+{
+    RpslObjects {
+        lines: lines.into_iter(),
+        class: String::new(),
+        name: String::new(),
+        body: String::new(),
+        new: false,
+    }
+}
+
+pub struct RpslExpr {
+    pub key: String,
+    pub expr: String,
+}
+
+pub fn expressions<I>(lines: I) -> impl Iterator<Item = RpslExpr>
+where
+    I: IntoIterator<Item = String>,
+{
+    lines.into_iter().filter_map(move |line| {
+        if !line.contains(':') {
+            error!("Invalid expression line: `{line}`.");
+            return None;
+        }
+        let mut parts = line.splitn(2, ':').map(str::trim);
+        let key = parts.next().unwrap().into();
+        let expr = parts.next().unwrap().into();
+        Some(RpslExpr { key, expr })
+    })
+}

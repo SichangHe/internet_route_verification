b804e18d5b4c747060026b41d8c2c6359cd90f1b
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-07-17 12:59:41 +0800

    load AsRelDb from text file and iterator

--- route_verification/as_rel/src/lib.rs
+++ route_verification/as_rel/src/lib.rs
@@ -1,6 +1,7 @@
 //! This module is originally ported from
 //! <https://github.com/cunha/measurements/blob/main/metadata/as-relationships/asrel.py>.
 use std::{
+    error::Error,
     fs::File,
     io::{prelude::*, BufReader},
     path::Path,
@@ -30,22 +31,58 @@ impl AsRelDB {
     }
 
     fn do_load(path: &Path) -> Result<Self> {
-        let mut source2dest = HashMap::new();
         let file = File::open(path)?;
         let reader = BufReader::new(BzDecoder::new(file));
-        for line in reader.lines() {
-            let line = line?;
-            if line.starts_with('#') {
-                continue;
-            }
-            let (key, relationship) = try_parse_as_rel(&line)?;
-            source2dest.insert(key, relationship);
+        Self::from_maybe_lines(reader.lines())
+    }
+
+    pub fn load_bz<P>(path: P) -> Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref();
+        debug!("Loading AS relationship database `{path:?}`.");
+        Self::do_load_bz(path).with_context(|| format!("loading `{path:?}` for AsRelDB"))
+    }
+
+    fn do_load_bz(path: &Path) -> Result<Self> {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        Self::from_maybe_lines(reader.lines())
+    }
+
+    pub fn from_lines<I, S>(lines: I) -> Result<Self>
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<str>,
+    {
+        let mut source2dest = HashMap::new();
+        for line in lines {
+            add_line_to_source2dest(line.as_ref(), &mut source2dest)?;
         }
+        Ok(Self::new(source2dest))
+    }
+
+    pub fn from_maybe_lines<I, S, E>(lines: I) -> Result<Self>
+    where
+        I: IntoIterator<Item = Result<S, E>>,
+        S: AsRef<str>,
+        E: Error + Sync + Send + 'static,
+    {
+        let mut source2dest = HashMap::new();
+        for maybe_line in lines {
+            let line = maybe_line?;
+            add_line_to_source2dest(line.as_ref(), &mut source2dest)?;
+        }
+        Ok(Self::new(source2dest))
+    }
+
+    fn new(source2dest: HashMap<(u64, u64), Relationship>) -> AsRelDB {
         debug!(
             "Loaded AS relationship database with {} links.",
             source2dest.len()
         );
-        Ok(Self { source2dest })
+        Self { source2dest }
     }
 
     pub fn get(&self, as1: u64, as2: u64) -> Option<Relationship> {
@@ -54,6 +91,17 @@ impl AsRelDB {
             None => self.source2dest.get(&(as2, as1)).map(|rel| rel.reversed()),
         }
     }
+}
+
+fn add_line_to_source2dest(
+    line: &str,
+    source2dest: &mut HashMap<(u64, u64), Relationship>,
+) -> Result<()> {
+    if !line.starts_with('#') {
+        let (key, relationship) = try_parse_as_rel(line)?;
+        source2dest.insert(key, relationship);
+    }
+    Ok(())
 }
 
 pub fn try_parse_as_rel(line: &str) -> Result<((u64, u64), Relationship)> {
--- route_verification/as_rel/src/tests.rs
+++ route_verification/as_rel/src/tests.rs
@@ -19,3 +19,23 @@ fn parse_lines() -> Result<()> {
 fn expected_as_rels() -> [((u64, u64), Relationship); 2] {
     [((323, 56), P2C), ((509, 9205), P2P)]
 }
+
+const FILE_LINES: &str = "141760|63927|0
+# input clique: 174 209 286 701 1239 1299 2828 2914 3257 3320 3356 3491 5511 6453 6461 6762 6830 7018 12956
+# IXP ASes: 1200 4635 5507 6695 7606 8714 9355 9439 9560 9722 9989 11670 15645 17819 18398 21371 24029 24115 24990 35054 40633 42476 43100 47886 48850 50384 55818 57463
+# source:topology|BGP|20230701|ripe|rrc00
+203188|12741|0
+63927|58430|0
+1239|4657|-1
+2914|4657|-1
+";
+
+#[test]
+fn parse_mini_file() -> Result<()> {
+    let db = AsRelDB::from_lines(FILE_LINES.lines())?;
+    assert_eq!(db.get(203188, 12741), Some(P2P));
+    assert_eq!(db.get(58430, 63927), Some(P2P));
+    assert_eq!(db.get(1239, 4657), Some(P2C));
+    assert_eq!(db.get(4657, 2914), Some(C2P));
+    Ok(())
+}

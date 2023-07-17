3964d1698b607f06c8a1bcd88b3fad5f19a35988
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-07-17 14:44:21 +0800

    start implementing up_down_hill_stats

--- route_verification/bgp/src/stats.rs
+++ route_verification/bgp/src/stats.rs
@@ -50,7 +50,7 @@ impl Compare {
 }
 
 /// Using [u32] so it is easy to put into a dataframe later.
-#[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Debug, Default)]
 pub struct AsStats {
     pub import_ok: u32,
     pub export_ok: u32,
@@ -58,4 +58,64 @@ pub struct AsStats {
     pub export_skip: u32,
     pub import_err: u32,
     pub export_err: u32,
+}
+
+pub fn up_down_hill_stats<'a, I>(reports_iter: I, db: &AsRelDb)
+where
+    I: IntoIterator<Item = &'a Vec<Report>>,
+{
+    let mut result = UpDownHillStats::default();
+    for reports in reports_iter {
+        for report in reports {
+            match report {
+                GoodImport { from, to } => match db.get(from, to) {
+                    Some(P2C) => todo!(),
+                    Some(P2P) => todo!(),
+                    Some(C2P) => todo!(),
+                    None => todo!(),
+                },
+                GoodExport { from, to } => todo!(),
+                GoodSingleExport { from } => todo!(),
+                NeutralImport { from, to, items } => todo!(),
+                NeutralExport { from, to, items } => todo!(),
+                NeutralSingleExport { from, items } => todo!(),
+                AsPathPairWithSet { from, to } => todo!(),
+                SetImport { from, to } => todo!(),
+                SetExport { from, to } => todo!(),
+                SetSingleExport { from } => todo!(),
+                BadImport { from, to, items } => todo!(),
+                BadExport { from, to, items } => todo!(),
+                BadSingeExport { from, items } => todo!(),
+            }
+        }
+    }
+}
+
+/// Using [u32] so it is easy to put into a dataframe later.
+#[derive(Clone, Debug, Default)]
+pub struct UpDownHillStats {
+    pub good_up_import: u32,
+    pub good_down_import: u32,
+    pub good_peer_import: u32,
+    pub good_other_import: u32,
+    pub good_up_export: u32,
+    pub good_down_export: u32,
+    pub good_peer_export: u32,
+    pub good_other_export: u32,
+    pub neutral_up_import: u32,
+    pub neutral_down_import: u32,
+    pub neutral_peer_import: u32,
+    pub neutral_other_import: u32,
+    pub neutral_up_export: u32,
+    pub neutral_down_export: u32,
+    pub neutral_peer_export: u32,
+    pub neutral_other_export: u32,
+    pub bad_up_import: u32,
+    pub bad_down_import: u32,
+    pub bad_peer_import: u32,
+    pub bad_other_import: u32,
+    pub bad_up_export: u32,
+    pub bad_down_export: u32,
+    pub bad_peer_export: u32,
+    pub bad_other_export: u32,
 }

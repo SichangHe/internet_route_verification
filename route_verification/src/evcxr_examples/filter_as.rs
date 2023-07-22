b09802547c9282a8f21d7f02d372b5e10030581f
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-07-22 10:54:10 +0800

    write AS139609 non-neutral reports to file

--- route_verification/src/evcxr_examples/filter_as.rs
+++ route_verification/src/evcxr_examples/filter_as.rs
@@ -30,5 +30,32 @@ fn reports_for_paths_containing_certain_as(
         .join("\n");
     File::create("AS139609_reports.txt")?.write_all(all_display.as_bytes());
 
+    let mut all_non_neutral = filtered_bgp_lines
+        .par_iter()
+        .filter_map(|line| {
+            line.report
+                .as_ref()
+                .unwrap()
+                .iter()
+                .any(|report| {
+                    !matches!(
+                        report,
+                        Report::NeutralImport {
+                            from: _,
+                            to: _,
+                            items: _,
+                        } | Report::NeutralExport {
+                            from: _,
+                            to: _,
+                            items: _,
+                        } | Report::NeutralSingleExport { from: _, items: _ }
+                    )
+                })
+                .then(|| line.display_str())
+        })
+        .collect::<Vec<_>>()
+        .join("\n");
+    File::create("AS139609_non_neutral_reports.txt")?.write_all(all_non_neutral.as_bytes());
+
     Ok(())
 }

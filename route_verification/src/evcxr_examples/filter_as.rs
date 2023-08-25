519a1c71ed2b210cf45807bed7a20c2dba140967
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-08-25 10:48:19 +0800

    reports_for_paths_containing_certain_as use less memory

--- route_verification/src/evcxr_examples/filter_as.rs
+++ route_verification/src/evcxr_examples/filter_as.rs
@@ -3,16 +3,16 @@ use super::*;
 fn reports_for_paths_containing_certain_as(
     an: u64,
     query: &QueryDump,
-    bgp_lines: &Vec<Line>,
+    bgp_lines: &mut Vec<Line>,
     db: &AsRelDb,
 ) -> Result<()> {
-    let mut filtered_bgp_lines: Vec<Line> = bgp_lines
-        .par_iter()
+    let mut filtered_bgp_lines: Vec<&mut Line> = bgp_lines
+        .par_iter_mut()
         .filter_map(|line| {
             line.compare
                 .as_path
                 .contains(&AsPathEntry::Seq(an))
-                .then(|| line.clone())
+                .then_some(line)
         })
         .collect();
     println!("{}", filtered_bgp_lines.len());
@@ -25,39 +25,37 @@ fn reports_for_paths_containing_certain_as(
         line.display();
     }
 
-    let mut all_display = filtered_bgp_lines
-        .par_iter()
-        .map(|line| line.display_str())
-        .collect::<Vec<_>>()
-        .join("\n");
-    File::create(format!("AS{an}_reports.txt"))?.write_all(all_display.as_bytes());
+    let mut target = File::create(format!("AS{an}_non_skip_reports.txt"))?;
 
-    let mut all_non_skip = filtered_bgp_lines
-        .par_iter()
-        .filter_map(|line| {
-            line.report
-                .as_ref()
-                .unwrap()
-                .iter()
-                .any(|report| {
-                    !matches!(
-                        report,
-                        Report::SkipImport {
-                            from: _,
-                            to: _,
-                            items: _,
-                        } | Report::SkipExport {
-                            from: _,
-                            to: _,
-                            items: _,
-                        } | Report::SkipSingleExport { from: _, items: _ }
-                    )
-                })
-                .then(|| line.display_str())
-        })
-        .collect::<Vec<_>>()
-        .join("\n");
-    File::create(format!("AS{an}_non_skip_reports.txt"))?.write_all(all_non_skip.as_bytes());
+    for chunk in filtered_bgp_lines.chunks(256) {
+        let mut all_non_skip = chunk
+            .par_iter()
+            .filter_map(|line| {
+                line.report
+                    .as_ref()
+                    .unwrap()
+                    .iter()
+                    .any(|report| {
+                        !matches!(
+                            report,
+                            Report::SkipImport {
+                                from: _,
+                                to: _,
+                                items: _,
+                            } | Report::SkipExport {
+                                from: _,
+                                to: _,
+                                items: _,
+                            } | Report::SkipSingleExport { from: _, items: _ }
+                        )
+                    })
+                    .then(|| line.display_str())
+            })
+            .collect::<Vec<_>>()
+            .join("\n");
+        all_non_skip.push('\n');
+        target.write_all(all_non_skip.as_bytes());
+    }
 
     let mut line = filtered_bgp_lines[0].clone();
 

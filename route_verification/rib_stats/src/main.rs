34e8184c11ce4d94656aa039cc974028e907295b
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-12-23 15:38:42 +0800

    up to getting rib file paths

--- route_verification/rib_stats/src/main.rs
+++ route_verification/rib_stats/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::read_dir;
+
+use rayon::prelude::*;
+use route_verification::{as_rel::AsRelDb, bgp::QueryIr, ir::Ir};
+
 fn main() {
-    println!("Hello, world!");
+    let db = AsRelDb::load_bz("data/20230701.as-rel.bz2").unwrap();
+    let parsed = Ir::pal_read("parsed_all").unwrap();
+    let query = QueryIr::from_ir_and_as_relationship(parsed, &db);
+
+    let rib_files = read_dir("data/ribs")
+        .unwrap()
+        .map(|maybe_entry| maybe_entry.unwrap().path())
+        .filter(|path| path.is_file() && (path.ends_with(".gz") || path.ends_with(".bz2")))
+        .collect::<Vec<_>>();
 }

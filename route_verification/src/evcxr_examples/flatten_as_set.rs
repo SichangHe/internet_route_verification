e29d04dd57693a862efe401e023e33651b964a8b
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-01-10 16:22:06 +0800

    fix AS Set flatten script & get depth

--- route_verification/src/evcxr_examples/flatten_as_set.rs
+++ route_verification/src/evcxr_examples/flatten_as_set.rs
@@ -8,24 +8,26 @@ fn flatten_as_sets(query: QueryIr) -> Result<()> {
         visited_sets: &mut HashSet<String>,
         set_members: &[String],
         as_sets: &HashMap<String, AsSet>,
-    ) {
+    ) -> usize {
+        let mut max_depth = 0;
+
         for set_member in set_members {
-            if let Some(set) = as_sets.get(set_member) {
-                as_set.extend(set.members.iter().copied());
-                for name in &set.set_members {
-                    if !visited_sets.contains(name) {
-                        visited_sets.insert(name.to_string());
-                        if let Some(set) = as_sets.get(name) {
-                            flatten(as_set, visited_sets, &set.set_members, as_sets)
-                        }
-                    }
+            if !visited_sets.contains(set_member) {
+                visited_sets.insert(set_member.to_string());
+                if let Some(set) = as_sets.get(set_member) {
+                    as_set.extend(set.members.iter().copied());
+
+                    let depth = 1 + flatten(as_set, visited_sets, &set.set_members, as_sets);
+                    max_depth = max_depth.max(depth);
                 }
             }
         }
+
+        max_depth
     }
 
     let start = Instant::now();
-    let as_sets: HashMap<String, HashSet<u32>> = query
+    let as_sets: HashMap<String, (HashSet<u32>, usize)> = query
         .as_sets
         .par_iter()
         .map(|(name, set)| {
@@ -35,10 +37,9 @@ fn flatten_as_sets(query: QueryIr) -> Result<()> {
 
             let mut visited = HashSet::with_capacity(set.set_members.len() * 8);
             visited.insert(name.to_string());
-            visited.extend(set.set_members.iter().map(ToString::to_string));
-            flatten(&mut members, &mut visited, &set.set_members, &query.as_sets);
+            let depth = flatten(&mut members, &mut visited, &set.set_members, &query.as_sets);
 
-            (name.to_owned(), members)
+            (name.to_owned(), (members, depth))
         })
         .collect();
     println!(
@@ -49,9 +50,9 @@ fn flatten_as_sets(query: QueryIr) -> Result<()> {
 
     {
         let mut as_set_file = BufWriter::new(File::create("as_sets.txt")?);
-        for (num, as_set) in &as_sets {
+        for (num, (as_set, _depth)) in &as_sets {
             as_set_file.write_all(num.to_string().as_bytes());
-            as_set_file.write_all(b":");
+            as_set_file.write_all(b";");
             for (index, member) in as_set.iter().enumerate() {
                 if index > 0 {
                     as_set_file.write_all(b",");
@@ -65,11 +66,13 @@ fn flatten_as_sets(query: QueryIr) -> Result<()> {
 
     {
         let mut as_set_sizes_file = BufWriter::new(File::create("as_set_sizes.csv")?);
-        as_set_sizes_file.write_all(b"as_set,size\n")?;
-        for (num, as_set) in &as_sets {
+        as_set_sizes_file.write_all(b"as_set,size,depth\n")?;
+        for (num, (as_set, depth)) in &as_sets {
             as_set_sizes_file.write_all(num.to_string().as_bytes());
             as_set_sizes_file.write_all(b",");
             as_set_sizes_file.write_all(as_set.len().to_string().as_bytes());
+            as_set_sizes_file.write_all(b",");
+            as_set_sizes_file.write_all(depth.to_string().as_bytes());
             as_set_sizes_file.write_all(b"\n");
         }
         as_set_sizes_file.flush()?;

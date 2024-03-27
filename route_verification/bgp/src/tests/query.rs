4f3d23da0f2451b4a8c61db6023ba1d017a6fc79
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-03-27 15:21:15 +0800

    add `SpecExportCustomers`&`SpecImportCustomer` test case

--- route_verification/bgp/src/tests/query.rs
+++ route_verification/bgp/src/tests/query.rs
@@ -8,7 +8,7 @@ fn psedo_customer_set() -> Result<()> {
     let ir = ir()?;
     let db = as_relationship_db()?;
     let actual = HashMap::from_iter(QueryIr::from_ir_and_as_relationship(ir, &db).as_sets);
-    assert_eq!(actual, expected_as_sets());
+    assert_eq!(expected_as_sets(), actual);
     Ok(())
 }
 
@@ -25,6 +25,12 @@ fn expected_as_sets() -> HashMap<String, QueryAsSet> {
         },
         "c#1239".into()=> QueryAsSet {
             body: "".into(), members: HashSet::from([3130]), unrecorded_members: vec![], is_any: false
+        },
+        "c#9063".into()=> QueryAsSet {
+            body: "".into(), members: HashSet::from([196763]), unrecorded_members: vec![], is_any: false
+        },
+        "c#196763".into()=> QueryAsSet {
+            body: "".into(), members: HashSet::from([60351]), unrecorded_members: vec![], is_any: false
         }
     }
 }

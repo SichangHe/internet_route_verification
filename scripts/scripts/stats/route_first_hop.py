225753fd8d8b050dae1cfcc55b14634e56f58d83
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-04-03 15:52:26 +0800

    attempt dask

--- scripts/scripts/stats/route_first_hop.py
+++ scripts/scripts/stats/route_first_hop.py
@@ -5,7 +5,7 @@ This takes more RAM than ordinary laptops have.
 
 from concurrent import futures
 
-import pandas as pd
+from dask import dataframe as dd
 
 from scripts import CsvFile
 from scripts.csv_files import route_first_hop_stats_all as FILES
@@ -15,22 +15,22 @@ TAGS = ("ok", "skip", "unrec", "meh", "err")
 
 
 def read_route_stats(file: CsvFile):
-    return pd.read_csv(file.path, dtype="uint16", engine="pyarrow")
+    return dd.read_csv(file.path, dtype="uint16", blocksize=None, engine="pyarrow")
 
 
-def read_all_route_stats() -> pd.DataFrame:
-    with futures.ProcessPoolExecutor() as executor:
-        return pd.concat(
-            (d for d in executor.map(read_route_stats, FILES) if len(d) > 0),
-            copy=False,
-        )
+def read_all_route_stats():
+    return dd.concat(
+        (d for d in [read_route_stats(file) for file in FILES] if len(d) > 0),
+    )
 
 
 def main() -> None:
     with futures.ThreadPoolExecutor() as executor:
         executor.map(CsvFile.download_if_missing, FILES)
 
-    df = read_all_route_stats()
+    df = dd.concat([read_route_stats(file) for file in FILES])
 
-    print(df.to_string())
-    print(df.describe().to_string())
+    print(df.describe().compute().to_string())
+
+
+main() if __name__ == "__main__ " else None

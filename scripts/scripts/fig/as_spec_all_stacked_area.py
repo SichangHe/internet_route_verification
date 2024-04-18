048acf74a0c086730dd2a85b32134439ed69621f
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-04-18 22:23:59 +0800

    extract AS stats df reading logic

--- scripts/scripts/fig/as_spec_all_stacked_area.py
+++ scripts/scripts/fig/as_spec_all_stacked_area.py
@@ -2,46 +2,28 @@
 """
 
 import re
-from concurrent import futures
 
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 
-from scripts import CsvFile, download_csv_files_if_missing
 from scripts.csv_fields import MODIFIED_SPECIAL_CASE_FIELDS as MODIFIED_TAGS
 from scripts.csv_fields import MODIFIED_SPECIAL_CASE_LABELS
 from scripts.csv_fields import SPECIAL_CASE_REPORT_ITEM_FIELDS as TAGS
-from scripts.csv_files import as_stats_all
 from scripts.fig import smart_sample
+from scripts.fig.dataframes import as_stats_all_df
 
-FILES = as_stats_all
 PORTS = ("import", "export")
 LEVELS = ("ok", "skip", "unrec", "meh", "err")
 
 
-def read_as_stats(file: CsvFile):
-    return pd.read_csv(
-        file.path,
-        index_col="aut_num",
-        usecols=[f"{port}_{level}" for port in PORTS for level in LEVELS]  # type: ignore
-        + list(TAGS)
-        + ["aut_num"],
-        engine="pyarrow",
-    )
-
-
 def plot() -> tuple[Figure, Axes, pd.DataFrame]:
-    with futures.ProcessPoolExecutor() as executor:
-        df = (
-            pd.concat(
-                (d for d in executor.map(read_as_stats, FILES) if len(d) > 0),
-                copy=False,
-            )
-            .groupby("aut_num")
-            .sum(engine="pyarrow")  # type: ignore
-        )
+    df = as_stats_all_df(
+        [f"{port}_{level}" for port in PORTS for level in LEVELS]
+        + list(TAGS)
+        + ["aut_num"]
+    )
 
     d = pd.DataFrame(
         {
@@ -99,8 +81,6 @@ def plot() -> tuple[Figure, Axes, pd.DataFrame]:
 
 
 def main():
-    download_csv_files_if_missing(FILES)
-
     fig, _, _ = plot()
 
     pdf_name = f"AS-all-special-case-percentages-stacked-area.pdf"

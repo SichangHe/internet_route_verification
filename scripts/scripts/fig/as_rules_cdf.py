907aab63bf4954532e175743cc683b547663a85e
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-04-22 21:28:04 +0800

    plot tier1s in AS rules CDF

--- scripts/scripts/fig/as_rules_cdf.py
+++ scripts/scripts/fig/as_rules_cdf.py
@@ -2,6 +2,7 @@
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
@@ -9,25 +10,92 @@ from matplotlib.figure import Figure
 from scripts.csv_files import as_neighbors_vs_rules
 
 FILE = as_neighbors_vs_rules
+TIER1S = set(
+    (
+        174,
+        209,
+        286,
+        # 701,
+        1239,
+        1299,
+        # 2828,
+        2914,
+        3257,
+        3320,
+        3356,
+        3491,
+        5511,
+        # 6453,
+        # 6461,
+        6762,
+        6830,
+        # 7018,
+        12956,
+    )
+)
+"""ASes without aut-num objects are commented out."""
 
 
 def plot() -> tuple[Figure, Axes]:
     df_raw = pd.read_csv(FILE.path)
+
     # Remove ASes not in IRR.
     df = df_raw.drop(df_raw[df_raw["import"] == -1].index)  # type: ignore
     df["rules"] = df["import"] + df["export"]
+
+    # CCDF plotting reference: `matplotlib/axes/_axes.py`.
+    cdf_data = np.array(df["rules"])
+    cdf_order = np.argsort(cdf_data)
+    cdf_data = cdf_data[cdf_order]
+    aut_num_sorted = np.asarray(df["aut_num"])[cdf_order]
+    cum_weights = 1 - ((1 + np.arange(len(cdf_data))) / len(cdf_data))
+
+    tier1labels, tier1cdf_data, tier1cum_weights = [], [], []
+    for aut_num, n_rules, cum_weight in zip(aut_num_sorted, cdf_data, cum_weights):
+        if aut_num in TIER1S:
+            tier1labels.append(f"AS{aut_num}")
+            tier1cdf_data.append(n_rules)
+            tier1cum_weights.append(cum_weight)
+
+    cdf_data = np.concatenate((cdf_data, np.asarray((cdf_data[-1],))))
+    cum_weights = np.concatenate((np.asarray((1,)), cum_weights))
 
     fig: Figure
     ax: Axes
     fig, ax = plt.subplots(figsize=(16, 9))
     fig.tight_layout()
-    ax.ecdf(df["rules"], complementary=True, linewidth=4, label="CCDF")
+    ax.plot(
+        cdf_data,
+        cum_weights,
+        drawstyle="steps-pre",
+        linewidth=4,
+        label="CCDF",
+    )
+    ax.scatter(
+        tier1cdf_data,
+        tier1cum_weights,
+        c="red",
+        s=400,
+        marker="x",
+        linewidth=4,
+        label="Tier-1 ASes",
+        zorder=10,
+    )
+    for label, x, y in zip(tier1labels, tier1cdf_data, tier1cum_weights):
+        ax.annotate(
+            label,
+            (x, y),
+            textcoords="offset points",
+            xytext=(-35, -30),  # Modify this to move text around.
+            ha="center",
+        )
     ax.set_xscale("log")
     ax.set_yscale("log")
     ax.set_xlabel("Number of Import/Export Rules", fontsize=36)
     ax.set_ylabel("Complementary Cumulative\nFraction of ASes", fontsize=36)
     ax.tick_params(axis="both", labelsize=32)
     ax.grid()
+    ax.legend()
     # ax.legend(loc="best", fontsize=36)
 
     # For checking.

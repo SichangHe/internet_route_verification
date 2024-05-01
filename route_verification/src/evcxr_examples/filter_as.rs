293ae596658c117ef502c5ba513d7e15746c6830
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-05-01 21:12:47 +0800

    update filter AS script after verbosity changes

--- route_verification/src/evcxr_examples/filter_as.rs
+++ route_verification/src/evcxr_examples/filter_as.rs
@@ -24,7 +24,12 @@ fn reports_for_paths_containing_certain_as(
             .par_iter()
             .filter_map(|line| {
                 let mut line = (*line).clone();
-                line.compare.verbosity = Verbosity::minimum_all();
+                line.compare.verbosity = Verbosity {
+                    all_err: true,
+                    record_set: true,
+                    record_community: true,
+                    ..Verbosity::minimum_all()
+                };
                 line.report = Some(line.compare.check_with_relationship(query, db));
                 line.report
                     .as_ref()

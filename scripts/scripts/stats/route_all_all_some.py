87518e0bcf7b8aa51326d53949e57740f1bcfa74
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-05-08 23:33:00 +0800

    route stats excluding skips

--- scripts/scripts/stats/route_all_all_some.py
+++ scripts/scripts/stats/route_all_all_some.py
@@ -11,6 +11,7 @@ from scripts.csv_files import route_stats_all
 
 PORTS = ("import", "export")
 TAGS = ("ok", "skip", "unrec", "meh", "err")
+NON_SKIP_TAGS = ("ok", "meh", "err")
 
 
 def main() -> None:
@@ -24,10 +25,11 @@ def main() -> None:
         n_route += len(df)
 
         df["total"] = sum((df[f"{port}_{tag}"] for tag in TAGS for port in PORTS))
+        df["total_non_skips"] = sum(
+            (df[f"{port}_{tag}"] for tag in NON_SKIP_TAGS for port in PORTS)
+        )
         for tag in TAGS:
-            d = df[
-                df[f"import_{tag}"] + df[f"export_{tag}"] == df["total"]
-            ]
+            d = df[df[f"import_{tag}"] + df[f"export_{tag}"] == df["total"]]
             count = len(d)
             counts[f"all_{tag}"] += count
             counts["all_same"] += count
@@ -35,8 +37,17 @@ def main() -> None:
             d = df[df[f"import_{tag}"] + df[f"export_{tag}"] > 0]
             counts[f"some_{tag}"] += len(d)
 
+        for tag in NON_SKIP_TAGS:
+            d = df[df[f"import_{tag}"] + df[f"export_{tag}"] == df["total_non_skips"]]
+            count = len(d)
+            counts[f"all_{tag}_non_skips"] += count
+            counts["all_same_non_skips"] += count
+
         for port in PORTS:
             df[f"total_{port}"] = sum((df[f"{port}_{tag}"] for tag in TAGS))
+            df[f"total_{port}_non_skips"] = sum(
+                (df[f"{port}_{tag}"] for tag in NON_SKIP_TAGS)
+            )
 
             for tag in TAGS:
                 d = df[
@@ -50,37 +61,59 @@ def main() -> None:
                 d = df[df[f"{port}_{tag}"] > 0]
                 counts[f"some_{port}_{tag}"] += len(d)
 
+            for tag in NON_SKIP_TAGS:
+                d = df[
+                    (df[f"total_{port}_non_skips"] != 0)
+                    & (df[f"{port}_{tag}"] == df[f"total_{port}_non_skips"])
+                ]
+                count = len(d)
+                counts[f"all_same_{port}_{tag}_non_skips"] += count
+                counts[f"all_same_{port}_non_skips"] += count
+
         del df
 
     print(f"{n_route} routes in total.")
     for tag in TAGS:
-        percentage = counts[f"all_{tag}"] * 100 / n_route
-        print(f"{counts[f"all_{tag}"]} all {tag}, {percentage:.1f}%.")
-    percentage = counts["all_same"] * 100 / n_route
-    print(f"{counts["all_same"]} all same status, {percentage:.1f}%.\n")
+        count = counts[f"all_{tag}"]
+        percentage = count * 100 / n_route
+        print(f"{count} all {tag}, {percentage:.1f}%.")
+    count = counts["all_same"]
+    percentage = count * 100 / n_route
+    print(f"{count} all same status, {percentage:.1f}%.\n")
 
+    for tag in NON_SKIP_TAGS:
+        count = counts[f"all_{tag}_non_skips"]
+        percentage = count * 100 / n_route
+        print(f"{count} all {tag}, excluding skips, {percentage:.1f}%.")
+    count = counts["all_same_non_skips"]
+    percentage = count * 100 / n_route
+    print(f"{count} all same status, excluding skips, {percentage:.1f}%.\n")
 
     for tag in TAGS:
-        percentage = counts[f"some_{tag}"] * 100 / n_route
-        print(f"{counts[f"some_{tag}"]} have {tag}, {percentage:.1f}%.")
-
+        count = counts[f"some_{tag}"]
+        percentage = count * 100 / n_route
+        print(f"{count} have {tag}, {percentage:.1f}%.")
 
     for port in PORTS:
         print()
 
         for tag in TAGS:
-            percentage = counts[f"all_same_{port}_{tag}"] / n_route * 100
-            print(
-                f"{counts[f"all_same_{port}_{tag}"]} all {tag} in {port}, {percentage:.1f}%."
-            )
-        percentage = counts[f"all_same_{port}"] / n_route * 100
-        print(f"{counts[f'all_same_{port}']} all same status in {port}, {percentage:.1f}%.\n")
+            count = counts[f"all_same_{port}_{tag}"]
+            percentage = count / n_route * 100
+            print(f"{count} all {tag} in {port}, {percentage:.1f}%.")
+        count = counts[f"all_same_{port}"]
+        percentage = count / n_route * 100
+        print(f"{count} all same status in {port}, {percentage:.1f}%.\n")
 
         for tag in TAGS:
-            percentage = counts[f"some_{port}_{tag}"] / n_route * 100
-            print(
-                f"{counts[f"some_{port}_{tag}"]} have {tag} in {port}, {percentage:.1f}%."
-            )
+            count = counts[f"some_{port}_{tag}"]
+            percentage = count / n_route * 100
+            print(f"{count} have {tag} in {port}, {percentage:.1f}%.")
+
+        for tag in NON_SKIP_TAGS:
+            count = counts[f"all_same_{port}_{tag}_non_skips"]
+            percentage = count / n_route * 100
+            print(f"{count} all {tag} in {port}, excluding skips, {percentage:.1f}%.")
 
 
 if __name__ == "__main__":

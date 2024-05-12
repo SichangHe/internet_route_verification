c3ddec80e1a9e9d414ae650db8cd88d877b70a38
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-05-12 11:01:03 +0800

    separate non-skip cases for AS stats #99

--- scripts/scripts/stats/as_all_all_some.py
+++ scripts/scripts/stats/as_all_all_some.py
@@ -8,6 +8,7 @@ from scripts.fig.dataframes import as_stats_all_df
 
 PORTS = ("import", "export")
 TAGS = ("ok", "skip", "unrec", "meh", "err")
+NON_SKIP_TAGS = ("ok", "meh", "err")
 
 
 def main() -> None:
@@ -17,39 +18,78 @@ def main() -> None:
         + list(SAFELIST_REPORT_ITEM_FIELDS)
     )
     df["safelisted"] = sum(df[tag] for tag in SAFELIST_REPORT_ITEM_FIELDS)
-    df["special"] = sum(df[f"{port}_meh"] for port in PORTS) - df["safelisted"]
+    df["relaxed"] = sum(df[f"{port}_meh"] for port in PORTS) - df["safelisted"]
     n_as = len(df)
     print(f"{n_as} ASes in total.")
 
     df["total"] = sum((df[f"{port}_{tag}"] for tag in TAGS for port in PORTS))
+    df["total_non_skips"] = sum(
+        (df[f"{port}_{tag}"] for tag in NON_SKIP_TAGS for port in PORTS)
+    )
+    df_non_skip = df[df["total_non_skips"] == df["total"]]
+    n_non_skip_as = len(df_non_skip)
+    print(f"{n_non_skip_as} ASes without skip or unrec.\n")
+
     df_all = {}
     count_all = 0
+    count_all_non_skip = 0
     for tag in ("ok", "skip", "unrec", "err"):
         df_all[tag] = df[df[f"import_{tag}"] + df[f"export_{tag}"] == df["total"]]
         count = df_all[tag].__len__()
         percentage = count * 100 / n_as
-        print(f"{count} all {tag}, {percentage:.1f}%.")
+        percentage_non_skip = 0
+        if tag in NON_SKIP_TAGS:
+            percentage_non_skip = count * 100 / n_non_skip_as
+            count_all_non_skip += count
+        print(
+            f"{count} all {tag}, {percentage:.1f}% among all, {percentage_non_skip:.1f}% excluding skips."
+        )
         count_all += count
-    for tag in ("special", "safelisted"):
+
+    for tag in ("relaxed", "safelisted"):
         df_all[tag] = df[df[tag] == df["total"]]
         count = df_all[tag].__len__()
         percentage = count * 100 / n_as
-        print(f"{count} all {tag}, {percentage:.1f}%.")
+        percentage_non_skip = count * 100 / n_non_skip_as
+        print(
+            f"{count} all {tag}, {percentage:.1f}% among all, {percentage_non_skip:.1f}% excluding skips."
+        )
         count_all += count
     percentage = count_all * 100 / n_as
-    print(f"{count_all} all same status, {percentage:.1f}%.\n")
+    percentage_non_skip = count_all_non_skip * 100 / n_non_skip_as
+    print(
+        f"{count_all} all same status, {percentage:.1f}% among all, {percentage_non_skip:.1f}% excluding skips.\n"
+    )
 
     df_some = {}
+    df_some_non_skip = {}
     for tag in TAGS:
         df_some[tag] = df[df[f"import_{tag}"] + df[f"export_{tag}"] > 0].dropna()
         count = df_some[tag].__len__()
         percentage = count * 100 / n_as
         print(f"{count} have {tag}, {percentage:.1f}%.")
-    for tag in ("special", "safelisted"):
+        if tag in NON_SKIP_TAGS:
+            df_some_non_skip[tag] = df_non_skip[
+                df_non_skip[f"import_{tag}"] + df_non_skip[f"export_{tag}"] > 0
+            ]
+            count = df_some_non_skip[tag].__len__()
+            percentage_non_skip = count * 100 / n_non_skip_as
+            print(
+                f"{count} excluding ASes with skips have {tag}, {percentage_non_skip:.1f}%."
+            )
+
+    for tag in ("relaxed", "safelisted"):
         df_some[tag] = df[df[tag] > 0]
         count = df_some[tag].__len__()
         percentage = count * 100 / n_as
         print(f"{count} have {tag}, {percentage:.1f}%.")
+
+        df_some_non_skip[tag] = df_non_skip[df_non_skip[tag] > 0]
+        count = df_some_non_skip[tag].__len__()
+        percentage_non_skip = count * 100 / n_non_skip_as
+        print(
+            f"{count} excluding ASes with skips have {tag}, {percentage_non_skip:.1f}%."
+        )
 
     for port in PORTS:
         print()

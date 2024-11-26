243e705a1813d486cecf0ff8d45269524662ce51
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2024-11-26 07:33:33 -0800

    chore: publish new rpsl-lexer version w/ rye to update README

---
+++ rpsl_lexer/publish.sh
@@ -0,0 +1,5 @@
+# Copy this to your shell to run.
+# <https://github.com/astral-sh/rye/issues/871#issuecomment-1989369240>.
+rye publish \
+    --repository rpsl-lexer \
+    --repository-url https://upload.pypi.org/legacy/
